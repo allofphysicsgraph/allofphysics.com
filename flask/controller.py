7dbbf89904bc589cedd720636db4da86189ccfce
Ben Payne <ben.is.located@gmail.com>
2024-09-07 12:40:50 +0000

    connect to neo4j in Python

--- flask/controller.py
+++ flask/controller.py
@@ -128,12 +128,69 @@ import validate_dimensions_sympy as vdim  # PDG
 
 sys.path.append("pg_library")
 import neo4j_query
+import list_of_valid
 
 # Database Credentials
 # "bolt" vs "neo4j" https://community.neo4j.com/t/different-between-neo4j-and-bolt/18498
 uri = "bolt://neo4j_docker:7687"
 # userName        = "neo4j"
 # password        = "test"
+
+# ORDERING: must come after constrain_id_to_be_unique
+# Connect to the neo4j database server
+neo4j_available = False
+while not neo4j_available:
+    print("TRACE: started while loop")
+    try:
+        graphDB_Driver = GraphDatabase.driver(uri)
+        neo4j_available = True
+        time.sleep(1)
+    except ValueError:
+        print("waiting 5 seconds for neo4j connection")
+        time.sleep(5)
+
+try:
+    with graphDB_Driver.session() as session:
+        # NO TIMING NEEDED HERE
+        list_of_derivation_IDs = session.write_transaction(
+            neo4j_query.constrain_unique_id
+        )
+        if list_of_derivation_IDs:
+            number_of_derivations = len(list_of_derivation_IDs)
+        else:  # list_of_derivation_IDs was "None"
+            number_of_derivations = 0
+
+except neo4j.exceptions.ClientError as er:
+    print("Neo4j exception: " + str(er))
+
+
+import importlib
+
+print(
+    "flask",
+    importlib.metadata.version("flask"),
+    "- https://flask.palletsprojects.com/en/3.0.x/",
+)
+print(
+    "secure", importlib.metadata.version("secure"), "- https://pypi.org/project/secure/"
+)
+print(
+    "wtforms",
+    importlib.metadata.version("wtforms"),
+    "- https://wtforms.readthedocs.io/en/3.1.x/",
+)
+print(
+    "werkzeug",
+    importlib.metadata.version("werkzeug"),
+    "- https://werkzeug.palletsprojects.com/en/3.0.x/",
+)
+print(
+    "flask_wtf",
+    importlib.metadata.version("flask_wtf"),
+    "- https://flask-wtf.readthedocs.io/en/1.2.x/",
+)
+print("neo4j", importlib.metadata.version("neo4j"), "- https://pypi.org/project/neo4j/")
+print("python", sys.version)
 
 
 # global proc_timeout
