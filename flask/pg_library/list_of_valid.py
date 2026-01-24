15445ca3ece27208d3ce46ce714b3399c30ffc49
Ben Payne <ben.is.located@gmail.com>
2026-01-24 18:36:13 -0500

    now with abbreviations

--- flask/pg_library/list_of_valid.py
+++ flask/pg_library/list_of_valid.py
@@ -1,5 +1,9 @@
 symbol_categories = ["operation", "relation", "scalar", "vector", "matrix"]
 
+scalar_scope = ["real", "complex", "integer", "arbitrary"]
+
+scalar_domain = ["any", "positive", "negative", "non-negative"]
+
 node_types = [
     "derivation",
     "step",
@@ -11,12 +15,12 @@ node_types = [
 
 
 dimension_mass_units = [
-    "kilogram",
-    "gram",
+    "kilogram (kg)",  # https://www.wikidata.org/wiki/Q11570 and https://en.wikipedia.org/wiki/Kilogram
+    "gram (g)",
     "stone",  # https://en.wikipedia.org/wiki/Stone_(unit)
     "ton",  # https://en.wikipedia.org/wiki/Long_ton
-    "ounce",  # https://en.wikipedia.org/wiki/Ounce
-    "pound",
+    "ounce (oz)",  # https://en.wikipedia.org/wiki/Ounce
+    "pound (lb)",
 ]  # https://en.wikipedia.org/wiki/Pound_(mass)
 
 dimension_time_units = [
@@ -25,33 +29,44 @@ dimension_time_units = [
     "fortnight",
     "week",
     "day",
+    "sol",  # https://en.wikipedia.org/wiki/Mars_sol
     "hour",
     "minute",
-    "second",
-    "millisecond",
-    "microsecond",
-    "picosecond",
-    "femptosecond",
+    "second (s)",  # https://en.wikipedia.org/wiki/Second
+    "millisecond (ms)",
+    "microsecond (us)",
+    "picosecond (ps)",
+    "femptosecond (fs)",
 ]
 
 # https://en.wikipedia.org/wiki/Length
 dimension_length_units = [
-    "meter",
-    "centimeter",
-    "millimeter",
+    "kilometer (km)",
+    "meter (m)",  # https://en.wikipedia.org/wiki/Metre
+    "centimeter (cm)",
+    "millimeter (mm)",
+    "light-year (ly)",  # https://en.wikipedia.org/wiki/Light-year
+    "parsec (pc)",  # https://en.wikipedia.org/wiki/Parsec
+    "hand",  # https://en.wikipedia.org/wiki/Hand_(unit)
     "feet",
     "foot",  # https://en.wikipedia.org/wiki/Foot_(unit)
     "furlong",  # https://en.wikipedia.org/wiki/Furlong
-    "mile",  # https://en.wikipedia.org/wiki/Mile
+    "mile (mi)",  # https://en.wikipedia.org/wiki/Mile
     "league",  # https://en.wikipedia.org/wiki/League_(unit)
     "fathom",  # https://en.wikipedia.org/wiki/Fathom
     "nautical mile",  # https://en.wikipedia.org/wiki/Nautical_mile
     "yard",  # https://en.wikipedia.org/wiki/Yard
 ]
 
-dimension_temperature_units = ["fahrenheit", "celsius", "kelvin"]
+dimension_temperature_units = [
+    "fahrenheit (F)",
+    "celsius (C)",
+    "kelvin (K)",  # https://en.wikipedia.org/wiki/Kelvin
+]
 dimension_electric_charge_units = ["coulomb"]  # https://en.wikipedia.org/wiki/Coulomb
-dimension_amount_of_substance_units = ["mol"]
+dimension_amount_of_substance_units = [
+    "mol"
+]  # https://en.wikipedia.org/wiki/Mole_(unit)
 dimension_luminous_intensity_units = ["candela"]
 
 
@@ -60,3 +75,4 @@ dimension_luminous_intensity_units = ["candela"]
 # https://en.wikipedia.org/wiki/Equals_sign
 # https://en.wikipedia.org/wiki/Glossary_of_mathematical_symbols
 
+# EOF
