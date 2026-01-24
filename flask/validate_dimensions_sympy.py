42ae583df2541d660dcc78cded58ead74646f57d
Ben Payne <ben.is.located@gmail.com>
2026-01-23 20:50:00 -0500

    removed line

--- flask/validate_dimensions_sympy.py
+++ flask/validate_dimensions_sympy.py
@@ -32,7 +32,6 @@ import sympy.physics.units  # type: ignore
 from sympy.vector import cross, dot  # type: ignore
 from sympy.vector.deloperator import Del  # type: ignore
 
-
 logger = logging.getLogger(__name__)
 
 # https://pymotw.com/3/doctest/
